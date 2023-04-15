520db2bcbd80ea834b3a555af56086ee8e4bf11a
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-04-15 09:50:16 -0500

    Add entity country

---
+++ Sales/Sales.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        public string Name { get; set; } = null!;
+    }
+}
