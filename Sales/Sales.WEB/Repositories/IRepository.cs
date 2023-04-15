20cc2457786949640d155a9edb1a98f0e73674c2
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-04-15 11:41:38 -0500

    Begin countries CRUD

---
+++ Sales/Sales.WEB/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Sales.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+
+        Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> PostAsync<T, TResponse>(string url, T model);
+    }
+}
