8e272edf887345c99ee9b7eeb46213f1529bcbd1
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-04-15 10:27:36 -0500

    Complete country entity

--- Sales/Sales.API/Data/DataContext.cs
+++ Sales/Sales.API/Data/DataContext.cs
@@ -10,5 +10,11 @@ namespace Sales.API.Data
         }
 
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
     }
 }
