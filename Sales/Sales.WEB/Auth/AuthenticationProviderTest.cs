b4dde24448e5a08dfd703096675205c63118023c
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-05-06 09:35:17 -0500

    Basic authentication

---
+++ Sales/Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var zuluUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Ruperto Aleczaxder"),
+                new Claim("LastName", "Zulu"),
+                new Claim(ClaimTypes.Name, "Ruperto_Aleczaxder@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+    }
+}
